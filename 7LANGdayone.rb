58262961b7578bd59d204d252bdd664ff0360d93
ru.talisman.tal <talismanoff1990@yandex.ru>
2015-11-10 01:09:48 +0300

    первый день знакомства с Ruby

---
+++ 7LANGdayone.rb
@@ -0,0 +1,35 @@
+# your code goes here
+#1 puts "Hello World"
+#str = "Hello, Ruby"
+#puts str.index("Ruby");
+#i = 0
+#while i < 10
+#	puts "Alex"
+#	i = i + 1
+#end
+=begin
+i = 1
+while i <= 10
+	puts "This is sequence #{i}"
+	i = i + 1
+end
+=end
+=begin
+secret = 0 + rand(10)
+guess = gets.to_i
+
+while guess != secret
+	puts "otgadai"
+	puts "#{secret}"
+	guess = gets.to_i
+	puts "vasha dogadka #{guess}"
+	if guess < secret
+		puts "bolshe"
+	end
+	if guess > secret
+		puts "menshe"
+	end
+end
+puts "Vi viigrali"
+=end
+	
